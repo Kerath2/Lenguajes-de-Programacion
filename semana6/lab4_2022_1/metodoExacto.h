40b7b2e444d182c70b2193274d50be038f03dca2
Gerardo Tacay <57574673+Kerath2@users.noreply.github.com>
2023-05-24 19:12:38 -0500

    Delete semana6 directory

--- semana6/lab4_2022_1/metodoExacto.h
+++
@@ -1,7 +0,0 @@
-#ifndef METODO_EXACTO
-#include <fstream>
-using namespace std; 
-void cargaClientes(int*&cli_dni,char **&cli_nombre,char *&cli_categoria);
-void leercadena(ifstream &arch,char * &cad);
-#endif // !METODO_EXACTO
-
--- semana6/repasoLab/2022-1/funciones.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCIONES
-#define FUNCIONES
-#include "operadores.h"
-
-void inicializa(Estructura_ClienteRegistrado * clientes,int tam );
-#endif // !FUNCIOONES
-
